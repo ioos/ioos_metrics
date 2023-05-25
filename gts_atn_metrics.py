05f0b87098d259d662a3e81c61451f9af50764ca
Mathew Biddle <8480023+MathewBiddle@users.noreply.github.com>
2023-05-25 11:06:15 -0400

    gts regionals extended back to 2018 run black on atn metrics testing reading bufr messages webpage testing

--- gts_atn_metrics.py
+++ gts_atn_metrics.py
@@ -7,65 +7,65 @@ from bs4 import BeautifulSoup
 # gather FY start/end dates for previous quarter
 fq = FiscalQuarter.current().prev_fiscal_quarter
 
-start_date = fq.start.strftime('%Y-%m-%d')
-end_date = fq.end.strftime('%Y-%m-%d')
+start_date = fq.start.strftime("%Y-%m-%d")
+end_date = fq.end.strftime("%Y-%m-%d")
 
-start = dt.datetime.strptime(start_date,'%Y-%m-%d')
-end = dt.datetime.strptime(end_date,'%Y-%m-%d')
+start = dt.datetime.strptime(start_date, "%Y-%m-%d")
+end = dt.datetime.strptime(end_date, "%Y-%m-%d")
 
 # recursively search the https index for bufr messages
-url = 'https://stage-ndbc-bufr.srv.axds.co/platforms/atn/smru/profiles/'
+url = "https://stage-ndbc-bufr.srv.axds.co/platforms/atn/smru/profiles/"
 
 html = requests.get(url).text
-soup = BeautifulSoup(html, 'html.parser')
+soup = BeautifulSoup(html, "html.parser")
 
 df_out = pd.DataFrame()
 
-for deployment in soup.find_all('a'):
+for deployment in soup.find_all("a"):
+    depl_url = url + deployment.text
+    depl_html = requests.get(depl_url).text
 
-  depl_url = url+deployment.text
-  depl_html = requests.get(depl_url).text
+    depl_soup = BeautifulSoup(depl_html, "html.parser")
 
-  depl_soup = BeautifulSoup(depl_html, 'html.parser')
+    # some content is not in an html node, so we have to parse line by line
+    files = depl_soup.get_text().split("\r\n")[1:-1]
 
-  # some content is not in an html node, so we have to parse line by line
-  files = depl_soup.get_text().split('\r\n')[1:-1]
+    for file in files:
+        content = file.split()
 
-  for file in files:
+        if ".bufr" in content[0]:
+            # save the index file information to DF.
+            fname = deployment.text + content[0]
+            df_file = pd.DataFrame(
+                {
+                    "fname": [fname],
+                    "date": pd.to_datetime([content[1] + "T" + content[2]]),
+                    "size": [content[3]],
+                }
+            )
 
-    content = file.split()
-
-    if '.bufr' in content[0]:
-      # save the index file information to DF.
-      df_file = pd.DataFrame(
-        {
-          'fname': [deployment.text+content[0]],
-          'date': pd.to_datetime([content[1]+"T"+content[2]]),
-          'size': [content[3]]
-        }
-      )
-
-      df_out = pd.concat([df_out, df_file])
+            df_out = pd.concat([df_out, df_file])
 
 
-print('{} total messages from ATN to GTS.'.format(df_out.shape[0]))
+print("{} total messages from ATN to GTS.".format(df_out.shape[0]))
 
 # mask for FY Quarter
-df_out = df_out.set_index("date")
-df_fq = df_out.sort_index()[fq.start:fq.end]
+df_out = df_out.set_index("date").sort_index()
+df_fq = df_out.sort_index()[fq.start : fq.end]
 
-print('For {} ATN sent {} records to GTS.'.format(fq, df_fq.shape[0]))
+string = "For {} ({} to {}) ATN sent {} records to GTS.".format(
+    fq, fq.start.strftime("%Y-%m-%d"), fq.end.strftime("%Y-%m-%d"), df_fq.shape[0]
+)
 
-#groupby month and save data
+print(string)
+
+# groupby month and save data
 group = df_out.groupby(pd.Grouper(freq="M"))
 
-s = group['fname'].count()
+s = group["fname"].count()
 
 s.index = s.index.to_period("M")
 
-s = s.rename('total')
+s = s.rename("total")
 
-s.to_csv('gts/GTS_ATN_monthly_totals.csv')
-
-
-
+s.to_csv("gts/GTS_ATN_monthly_totals.csv")
--- gts_regional_metrics.py
+++ gts_regional_metrics.py
@@ -2,42 +2,59 @@ import datetime as dt
 import pandas as pd
 from fiscalyear import *
 
-# gather FY start/end dates for previous quarter
-fq = FiscalQuarter.current().prev_fiscal_quarter
+start_date = dt.datetime(2018, 1, 1)
 
-start_date = fq.start.strftime('%Y-%m-%d')
-end_date = fq.end.strftime('%Y-%m-%d')
+for date in pd.date_range(start_date, dt.datetime.now(), freq="Q"):
+    year = int(date.strftime("%Y"))
+    month = int(date.strftime("%m"))
+    day = int(date.strftime("%d"))
 
-start = dt.datetime.strptime(start_date,'%Y-%m-%d')
-end = dt.datetime.strptime(end_date,'%Y-%m-%d')
+    fd = FiscalDate(year, month, day)
 
-# build an array for days between dates
-date_array = (start + dt.timedelta(days=x) for x in range(0, (end - start).days))
+    fq = FiscalQuarter(fd.fiscal_year, fd.fiscal_quarter)
 
-# get a unique list of year-months for url build
-months=[]
-for date_object in date_array:
-    months.append(date_object.strftime("%Y-%m"))
-months = sorted(set(months))
+    # gather FY start/end dates for previous quarter
+    # fq = FiscalQuarter.current().prev_fiscal_quarter
 
-df = pd.DataFrame(columns=['locationID', 'region', 'sponsor', 'met', 'wave'])
-for month in months:
-  url = 'https://www.ndbc.noaa.gov/ioosstats/rpts/%s_ioos_regional.csv' % month.replace("-","_")
-  print('Loading %s' % url)
-  df1 = pd.read_csv(url, dtype={'met':float, 'wave':float})
-  df1['time (UTC)'] = pd.to_datetime(month)
-  df = pd.concat([df,df1])
+    start_date = fq.start.strftime("%Y-%m-%d")
+    end_date = fq.end.strftime("%Y-%m-%d")
 
-df["time (UTC)"] = pd.to_datetime(df["time (UTC)"])
-# Remove time-zone info for easier plotting, it is all UTC.
-df["time (UTC)"] = df["time (UTC)"].dt.tz_localize(None)
+    start = dt.datetime.strptime(start_date, "%Y-%m-%d")
+    end = dt.datetime.strptime(end_date, "%Y-%m-%d")
 
-groups = df.groupby(pd.Grouper(key="time (UTC)", freq="M"))
+    # build an array for days between dates
+    date_array = (start + dt.timedelta(days=x) for x in range(0, (end - start).days))
 
-s = groups[['met','wave']].sum() # reducing the columns so the summary is digestable
-totals = s.assign(total=s["met"] + s["wave"])
-totals.index = totals.index.to_period("M")
+    # get a unique list of year-months for url build
+    months = []
+    for date_object in date_array:
+        months.append(date_object.strftime("%Y-%m"))
+    months = sorted(set(months))
 
-fname = 'gts/GTS_totals_FY%s_Q%s.csv' % (fq.fiscal_year,fq.fiscal_quarter)
+    df = pd.DataFrame(columns=["locationID", "region", "sponsor", "met", "wave"])
+    for month in months:
+        url = (
+            "https://www.ndbc.noaa.gov/ioosstats/rpts/%s_ioos_regional.csv"
+            % month.replace("-", "_")
+        )
+        print("Loading %s" % url)
+        df1 = pd.read_csv(url, dtype={"met": int, "wave": int})
+        df1["date"] = pd.to_datetime(month)
+        df = pd.concat([df, df1])
 
-totals.to_csv(fname)
+    df["date"] = pd.to_datetime(df["date"])
+    # Remove time-zone info for easier plotting, it is all UTC.
+    df["date"] = df["date"].dt.tz_localize(None)
+
+    groups = df.groupby(pd.Grouper(key="date", freq="M"))
+
+    s = groups[
+        ["met", "wave"]
+    ].sum()  # reducing the columns so the summary is digestable
+
+    totals = s.assign(total=s["met"] + s["wave"])
+    totals.index = totals.index.to_period("M")
+
+    fname = "gts/GTS_regional_totals_FY%s_Q%s.csv" % (fq.fiscal_year, fq.fiscal_quarter)
+
+    totals.to_csv(fname)
