e193ab247bb27519191a1c7852faaeb68561444d
Mathew Biddle <8480023+MathewBiddle@users.noreply.github.com>
2023-06-05 15:57:02 -0400

    adding GTS regional totals bar chart closes #21

--- website/create_gts_regional_landing_page.py
+++ website/create_gts_regional_landing_page.py
@@ -4,13 +4,14 @@ from jinja2 import Environment, FileSystemLoader
 import json
 import os
 import pandas as pd
-
+import plotly.express as px
+import plotly
 
 def write_html_index(template, configs, org_config):
     root = os.path.dirname(os.path.abspath(__file__))
     # root = path to output directory
     filename = os.path.join(root, "deploy", "gts_regional.html")
-    with open(filename, "w") as fh:
+    with open(filename, "w", encoding="utf-8") as fh:
         fh.write(template.render(org_config=org_config, configs=configs))
 
 
@@ -26,14 +27,58 @@ def write_templates(configs, org_config):
     template = load_template()
     write_html_index(template, configs, org_config)
 
+def timeseries_plot(output):
+    # returns plot as html string.
+    # might need to use plotly
+    # import plotly.express as px
+    #
+    # fig =px.scatter(x=range(10), y=range(10))
+    # fig = fig.write_html("path/to/file.html")
+    output["date"] = pd.to_datetime(output["date"])
+
+    fig = px.bar(output, x="date", y="total",
+                 title="Regional Messages sent to the GTS via NDBC")
+    #fig = fig.update_traces(textfont_size=12, textangle=0, textposition="outside", cliponaxis=False)
+    fig = fig.update_xaxes(
+        #ticklabelmode="period",
+        dtick="M3",
+        tickformat="%b\n%Y",
+        rangeslider_visible=True,
+        rangeselector={'buttons': [
+                dict(count=1, label="1m", step="month", stepmode="backward"),
+                dict(count=6, label="6m", step="month", stepmode="backward"),
+                dict(count=1, label="YTD", step="year", stepmode="todate"),
+                dict(count=1, label="1y", step="year", stepmode="backward"),
+                dict(step="all")]},
+        )
+    fig = plotly.io.to_html(fig, full_html=False)
+    #    print(fig)
+    # fig = fig.write_html(full_html=False)
+    # print(fig)
+    # fig, axs = plt.subplots(figsize=(6.4, 7), layout='constrained')
+    #
+    # axs.bar(output['date'], output['total'], width=8)
+    # axs.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
+    # axs.set_ylabel('Total messages')
+    #
+    # tmpfile = BytesIO()
+    # fig.savefig(tmpfile, format='png')
+    # encoded = base64.b64encode(tmpfile.getvalue()).decode('utf-8')
+    #
+    # fig = '<img src=\'data:image/png;base64,{}\'>'.format(encoded)
+
+    return fig
 
 def main(org_config):
     configs = dict()
+    output_all = pd.DataFrame()
 
     files = os.listdir(org_config["location_of_metrics"])
 
     files.remove('GTS_ATN_monthly_totals.csv')
     files = sorted(files)
+
+
 
     for f in files:
         filename = os.path.join(org_config["location_of_metrics"], f)
@@ -41,6 +86,8 @@ def main(org_config):
         f_out = filename.replace(".csv", ".html").replace(
             org_config["location_of_metrics"], "deploy"
         )
+
+        output_all = pd.concat([output_all, output], ignore_index=True)
 
         #output = output.style.set_table_styles([{'selector': 'td', 'props': 'text-align: right; font-weight: bold;'}])#properties(**{'text-align': 'right'})
 
@@ -59,6 +106,10 @@ def main(org_config):
         configs[key] = {'name': f_out,
                         'data': f,
                         'table': table}
+
+    fig = timeseries_plot(output_all)
+
+    configs['figure'] = fig
 
     # myKeys = list(configs.keys())
     # myKeys.sort()
