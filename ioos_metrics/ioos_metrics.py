3e39ef7dd9fc10ce271dddd8ecb521ce56915046
Filipe Fernandes <ocefpaf@gmail.com>
2024-02-13 16:58:21 -0300

    use random fake agent

--- ioos_metrics/ioos_metrics.py
+++ ioos_metrics/ioos_metrics.py
@@ -9,9 +9,11 @@ import warnings
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 
+ua = UserAgent()
 _HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
+    "User-Agent": ua.random,
 }
 
 
