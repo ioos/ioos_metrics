0bfdaba2eb629d207906eabbe3a6656b617099c0
Filipe Fernandes <ocefpaf@gmail.com>
2024-02-19 14:22:38 -0300

    test only the fast glider metric version

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -18,7 +18,7 @@ from ioos_metrics.ioos_metrics import (
     ioos_core_variables,
     mbon_projects,
     metadata_records,
-    ngdac_gliders,
+    ngdac_gliders_fast,
     ott_projects,
     qartod_manuals,
     regional_associations,
@@ -35,6 +35,12 @@ def df_previous_metrics():
 def test_previous_metrics(df_previous_metrics):
     assert isinstance(df_previous_metrics, pd.DataFrame)
     assert not df_previous_metrics.empty
+
+
+def test_ngdac_gliders_fast(df_previous_metrics):
+    num = ioos_metrics.ngdac_gliders_fast()
+    # New count should always be >= than the previous one.
+    assert num >= df_previous_metrics["NGDAC Glider Days"].iloc[-1]
 
 
 def test_ngdac_gliders(df_previous_metrics):
@@ -58,7 +64,7 @@ def test_if_metric_is_a_natural_number():
         ioos_core_variables,
         mbon_projects,
         metadata_records,
-        ngdac_gliders,
+        ngdac_gliders_fast,
         ott_projects,
         qartod_manuals,
         regional_associations,
